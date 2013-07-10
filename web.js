647a1df6e9cb2131daa4194b2c3a7f2c03cc291d
Richard Dulude <richard.dulude@gmail.com>
2013-07-10 21:57:33 +0000

    Revised to pick up index.html file

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer('test');
+
+var infile = "index.html";
+
+//load buffer with string from index.html
+buf = fs.readFileSync(infile, 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
